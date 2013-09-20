3c2076c0d88ef8ee84e4593910bee5dfe14447d2
Davide Magrin <magrin.davide@gmail.com>
2013-09-20 11:37:21 +0200

    Reverted some changes

--- animation.js
+++ animation.js
@@ -1,6 +1,1 @@
-// Fires when the document is loaded
-$(document).ready(function() {
-	// Gets the context (has to fetch the DOM node from the jQuery object)
-	var ctx = $("#animation").get(0).getContext("2d");
-	// Start the path
-});
+
