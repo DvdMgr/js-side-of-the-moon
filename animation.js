4a7c73360cb38d7b64bce0a4e123a3b93d4c36ea
Davide Magrin <magrin.davide@gmail.com>
2013-09-21 11:53:24 +0200

    Added rays in Prism and made them fade

--- animation.js
+++ animation.js
@@ -2,12 +2,26 @@
 var CANVASHEIGHT;
 var CANVASWIDTH;
 var incident;
-var spread;
-var INCIDENTRAYSNUMBER = 20;
-var INCIDENTRAYSPREAD = 50;
+var INCIDENTRAYSNUMBER = 10;
+var INCIDENTRAYSPREAD = 70;
 var incidencepointx;
 var incidencepointy;
+var prismrays;
+var PRISMRAYSSPREAD = 150;
+var prismrayscenterx;
+var prismrayscentery;
+
+// Controls the fading
+var FADINGRESOLUTION = 100;
+var FADINGPOINT = 0.75;// PERCENTAGE
+
+// Useful variables
 var thisline;
+var randommultiplier;
+var fadingpointx;
+var fadingpointy;
+var tractlengthx;
+var tractlengthy;
 // Fires when the document is fully loaded
 $(document).ready(function(){
 	var canvas = $("#animation").get(0);
@@ -16,9 +30,11 @@ $(document).ready(function(){
 	CANVASWIDTH = canvas.width;
 	drawPrism(ctx);
 	incidencepointx = CANVASWIDTH/9+CANVASWIDTH/3;
+	// Don't ask. Trigonometry is awesome.
 	incidencepointy = (4*CANVASHEIGHT-Math.sqrt(3)*CANVASWIDTH)/6+CANVASWIDTH*Math.sqrt(3)/6-2*CANVASWIDTH/9*Math.sqrt(3)/2;
 	initialize();
 	printIncidentLines(ctx);
+	printPrismLines(ctx);
 });
 
 // Pretty self explainatory
@@ -41,6 +57,14 @@ function initialize() {
 		incident[i] = new line(0, Math.random()*INCIDENTRAYSPREAD+CANVASHEIGHT*(2/3)-10, incidencepointx, incidencepointy, "#FFFFFF", 1);
 		//console.log(incident[i]);
 	}
+	// The rays in the prism
+	prismrays = new Array(INCIDENTRAYSNUMBER);
+	for (var i = 0; i < INCIDENTRAYSNUMBER; i++) {
+		randommultiplier = (Math.random()*2-1);
+		// Fugly
+		prismrays[i] = new line(incidencepointx, incidencepointy, incidencepointx+CANVASWIDTH/9-PRISMRAYSSPREAD/4*randommultiplier, incidencepointy-PRISMRAYSSPREAD/4*Math.sqrt(3)*randommultiplier, "#FFFFFF", 1);
+		console.log(prismrays[i]);
+	}
 }
 
 // Self explainatory
@@ -56,6 +80,28 @@ function printIncidentLines(ctx) {
 	ctx.stroke();
 }
 
+function printPrismLines(ctx) {
+	for (var i = 0; i < INCIDENTRAYSNUMBER; i++) {
+		//ctx.beginPath();
+		thisline = prismrays[i];
+		// Draws the lines with fading, dividing them in multiple tracts
+		fadingpointx = thisline.startx+(thisline.endx-thisline.startx)*FADINGPOINT;
+		fadingpointy = thisline.starty+(thisline.endy-thisline.starty)*FADINGPOINT;
+		tractlengthx = (fadingpointx - thisline.startx)/FADINGRESOLUTION;
+		tractlengthy = (fadingpointy - thisline.starty)/FADINGRESOLUTION;
+		for (var j = 0; j < FADINGRESOLUTION; j++) {
+			ctx.beginPath();
+			ctx.strokeStyle = thisline.color;
+			ctx.lineWidth = thisline.width;
+			ctx.globalAlpha = (FADINGRESOLUTION-j)/FADINGRESOLUTION;
+			ctx.moveTo(thisline.startx+(j)*tractlengthx, thisline.starty+(j)*tractlengthy);
+			ctx.lineTo(thisline.startx+(j+1)*tractlengthx, thisline.starty+(j+1)*tractlengthy);
+			ctx.stroke();		
+		}
+	}
+	// Restores globalAlpha
+	ctx.globalAlpha = 1;
+}
 
 // The line object
 function line(startx, starty, endx, endy, color, width) {
